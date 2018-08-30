28f79192da37e10d7ea4dd33563b206677bb0d9c
Arielle D <domingu4@gmail.com>
2018-08-30 16:26:01 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  return attendees.each{|badges| puts "Hello, my name is #{badges}."}
+  attendees.each{|badges| puts "Hello, my name is #{badges}."}
 end
 
 def assign_rooms
