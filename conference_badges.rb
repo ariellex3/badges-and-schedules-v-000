a888f7c547abc2c239b25767e72933e6310124d6
Arielle D <domingu4@gmail.com>
2018-08-30 16:10:15 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  ["Hello, my name is #{attendees[0]}.",
+  badges = ["Hello, my name is #{attendees[0]}.",
   "Hello, my name is #{attendees[1]}.",
   "Hello, my name is #{attendees[2]}.",
   "Hello, my name is #{attendees[3]}.",
